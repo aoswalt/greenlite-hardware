b61ac0f5526c5447861617a72cfa35e8e940fe11
Adam Oswalt <email@adamoswalt.com>
2016-10-02 22:23:25 +0000

    lights configuration

--- vertex-server/signals/config.py
+++ vertex-server/signals/config.py
@@ -1,20 +1,30 @@
-central_server_address = 'http://localhost:8000/'
-# central_server_address = 'http://10.0.0.21/'
+#central_server_address = 'http://localhost:8000/'
+central_server_address = 'http://10.0.0.21/'
 
-intersection_label = '_unset_'
+intersection_label = 'inter5'
 
 # light pin definitions
 light_pins = {
-  'a1r': (1, 1),
-  'a1y': (1, 2),
-  'a1g': (1, 3),
-  'a1gl': (1, 6),
-  'a2r': (3, 1),
-  'a2y': (3, 2),
-  'a2g': (3, 3),
-  'b1r': (2, 1),
-  'b1y': (2, 2),
-  'b1g': (2, 3),
+  'aly': (20,),
+  'alg': (18,),
+  'asr': (21,),
+  'asy': (16,),
+  'asg': (12,),
+  'bly': (13,),
+  'blg': (5,),
+  'bsr': (26,),
+  'bsy': (19,),
+  'bsg': (6,),
+  'cly': (8,),
+  'clg': (24,),
+  'csr': (1,),
+  'csy': (7,),
+  'csg': (23,),
+  'dly': (27,),
+  'dlg': (22,),
+  'dsr': (4,),
+  'dsy': (17,),
+  'dsg': (10,),
 }
 
 # map lights keys to directions
@@ -22,34 +32,67 @@ mapping_labels = {
     'roadA': {
         'L': (
             ('',),
-            ('',),
-            ('a1gl',),
+            ('aly',),
+            ('alg',),
         ),
         'S': (
-            ('a1r', 'a2r',),
-            ('a1y', 'a2y',),
-            ('a1g', 'a2g',),
+            ('asr',),
+            ('asy',),
+            ('asg',),
         ),
     },
     'roadB': {
-        'S': (
-            ('b1r',),
-            ('b1y',),
-            ('b1g',),
+        'L': (
+            ('',),
+            ('bly',),
+            ('blg',),
         ),
-    }
+        'S': (
+            ('bsr',),
+            ('bsy',),
+            ('bsg',),
+        ),
+    },
+    'roadC': {
+        'L': (
+            ('',),
+            ('cly',),
+            ('clg',),
+        ),
+        'S': (
+            ('csr',),
+            ('csy',),
+            ('csg',),
+        ),
+    },
+    'roadD': {
+        'L': (
+            ('',),
+            ('dly',),
+            ('dlg',),
+        ),
+        'S': (
+            ('dsr',),
+            ('dsy',),
+            ('dsg',),
+        ),
+    },
 }
 
 # fallback pattern
 disabled_pattern = {
     'signals': [
         {
-            'roadA': {},
-            'roadB': {'L':0, 'S':0, 'R':0},
-        },
-        {
             'roadA': {'L':0, 'S':0, 'R':0},
             'roadB': {},
+            'roadC': {'L':0, 'S':0, 'R':0},
+            'roadD': {},
+        },
+        {
+            'roadA': {},
+            'roadB': {'L':0, 'S':0, 'R':0},
+            'roadC': {},
+            'roadD': {'L':0, 'S':0, 'R':0},
         },
     ]
 }
